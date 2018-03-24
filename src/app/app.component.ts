bf29486b271f43b9cbbf8e0c5fa263c6f401c33f
Serhiy Vorobyov <svorobyov@seamgen.com>
2018-03-23 22:43:16 -0700

    unbroke the broke

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,13 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-<<<<<<< HEAD
 import { HistoryPage } from '../pages/history/history';
-=======
 import { NewGamePage } from '../pages/new-game/new-game';
->>>>>>> ad4d5ec3eea2176dc834d2e303d3be9c17768d35
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-<<<<<<< HEAD
   rootPage:any = HistoryPage;
-=======
-  rootPage:any = NewGamePage;
->>>>>>> ad4d5ec3eea2176dc834d2e303d3be9c17768d35
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
